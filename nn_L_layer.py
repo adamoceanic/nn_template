f1ea75c32363852d673b70becf88ddc3b5c879f9
Adam Jonze <ajze@users.noreply.github.com>
2018-01-28 21:05:48 +0000

    Update nn_L_layer.py

--- nn_L_layer.py
+++ nn_L_layer.py
@@ -13,40 +13,39 @@ get_ipython().magic('load_ext autoreload')
 get_ipython().magic('autoreload 2')
 
 np.random.seed(1)
-
-
-def initialize_parameters(n_x, n_h, n_y):
+   
+    
+ def initialize_parameters_deep(layer_dims):
     """
-    Argument:
-    n_x -- size of the input layer
-    n_h -- size of the hidden layer
-    n_y -- size of the output layer
+    Arguments:
+    layer_dims -- python array (list) containing the dimensions of each layer in our network
     
     Returns:
-    parameters -- python dictionary containing your parameters:
-                    W1 -- weight matrix of shape (n_h, n_x)
-                    b1 -- bias vector of shape (n_h, 1)
-                    W2 -- weight matrix of shape (n_y, n_h)
-                    b2 -- bias vector of shape (n_y, 1)
+    parameters -- python dictionary containing your parameters "W1", "b1", ..., "WL", "bL":
+                    Wl -- weight matrix of shape (layer_dims[l], layer_dims[l-1])
+                    bl -- bias vector of shape (layer_dims[l], 1)
     """
     
-    np.random.seed(1)
+    np.random.seed(3)
+    parameters = {}
+    L = len(layer_dims)  
     
-    W1 = np.random.randn(n_h,n_x) * 0.01
-    b1 = np.zeros((n_h,1))
-    W2 = np.random.randn(n_y,n_h) * 0.01
-    b2 = np.zeros((n_y,1))
+    ## He Initialization - will experiment with this
+    #for l in range(1, L + 1):
+        #parameters['W' + str(l)] = np.random.randn(layers_dims[l],layers_dims[l-1]) * np.sqrt(2/layers_dims[l-1])
+        #parameters['b' + str(l)] = np.zeros((layers_dims[l], 1))
+
+
     
-    assert(W1.shape == (n_h, n_x))
-    assert(b1.shape == (n_h, 1))
-    assert(W2.shape == (n_y, n_h))
-    assert(b2.shape == (n_y, 1))
-    
-    parameters = {"W1": W1,
-                  "b1": b1,
-                  "W2": W2,
-                  "b2": b2}
-    
+    for l in range(1, L):        
+        parameters['W' + str(l)] = np.random.randn(layer_dims[l],layer_dims[l-1]) * 0.01
+        parameters['b' + str(l)] = np.zeros((layer_dims[l],1))
+        
+        
+        assert(parameters['W' + str(l)].shape == (layer_dims[l], layer_dims[l-1]))
+        assert(parameters['b' + str(l)].shape == (layer_dims[l], 1))
+
+        
     return parameters
     
     
