d272eb0c80ae49e7c33566763efbd85eab66c681
Adam Jonze <ajze@users.noreply.github.com>
2018-01-29 00:08:05 +0000

    Update nn_L_layer.py

--- nn_L_layer.py
+++ nn_L_layer.py
@@ -140,8 +140,67 @@ def linear_activation_forward(A_prev, W, b, activation):
     assert(AL.shape == (1,X.shape[1]))
             
     return AL, caches
-          
+ 
+# ==================================================================
+## This needs generalising and testing.
+## Vectorised
+   
+def forward_propagation_with_dropout(X, parameters, keep_prob = 0.5):
+    """
+    Implements the forward propagation: LINEAR -> RELU + DROPOUT -> LINEAR -> RELU + DROPOUT -> LINEAR -> SIGMOID.
     
+    Arguments:
+    X -- input dataset, of shape (2, number of examples)
+    parameters -- python dictionary containing your parameters "W1", "b1", "W2", "b2", "W3", "b3":
+                    W1 -- weight matrix of shape (20, 2)
+                    b1 -- bias vector of shape (20, 1)
+                    W2 -- weight matrix of shape (3, 20)
+                    b2 -- bias vector of shape (3, 1)
+                    W3 -- weight matrix of shape (1, 3)
+                    b3 -- bias vector of shape (1, 1)
+    keep_prob - probability of keeping a neuron active during drop-out, scalar
+    
+    Returns:
+    A3 -- last activation value, output of the forward propagation, of shape (1,1)
+    cache -- tuple, information stored for computing the backward propagation
+    """
+    
+    np.random.seed(1)
+    
+    # retrieve parameters
+    W1 = parameters["W1"]
+    b1 = parameters["b1"]
+    W2 = parameters["W2"]
+    b2 = parameters["b2"]
+    W3 = parameters["W3"]
+    b3 = parameters["b3"]
+    
+    # LINEAR -> RELU -> LINEAR -> RELU -> LINEAR -> SIGMOID
+    Z1 = np.dot(W1, X) + b1
+    A1 = relu(Z1)
+    
+    D1 = np.random.rand(A1.shape[0], A1.shape[1])     
+    D1 = (D1 < keep_prob)                             
+    A1 = np.multiply(A1,D1)                           
+    A1 = np.divide(A1,keep_prob)                      
+    
+    Z2 = np.dot(W2, A1) + b2
+    A2 = relu(Z2)
+    
+    D2 = np.random.rand(A2.shape[0], A2.shape[1])     
+    D2 = (D2 < keep_prob)                             
+    A2 = np.multiply(A2,D2)                           
+    A2 = np.divide(A2,keep_prob)                      
+    
+    Z3 = np.dot(W3, A2) + b3
+    A3 = sigmoid(Z3)
+    
+    cache = (Z1, D1, A1, W1, b1, Z2, D2, A2, W2, b2, Z3, A3, W3, b3)
+    
+    return A3, cache 
+   
+# ================================================================== 
+
 def compute_cost(AL, Y):
     """
     Implement the cost function defined by equation (7).
@@ -286,7 +345,57 @@ def backward_propagation_with_regularization(X, Y, cache, lambd):
     return gradients
    
 # ==================================================================   
+ 
+# ==================================================================
+## This needs generalising to L layers etc   
    
+def backward_propagation_with_dropout(X, Y, cache, keep_prob):
+    """
+    Implements the backward propagation of our baseline model to which we added dropout.
+    
+    Arguments:
+    X -- input dataset, of shape (2, number of examples)
+    Y -- "true" labels vector, of shape (output size, number of examples)
+    cache -- cache output from forward_propagation_with_dropout()
+    keep_prob - probability of keeping a neuron active during drop-out, scalar
+    
+    Returns:
+    gradients -- A dictionary with the gradients with respect to each parameter, activation and pre-activation variables
+    """
+    
+    m = X.shape[1]
+    (Z1, D1, A1, W1, b1, Z2, D2, A2, W2, b2, Z3, A3, W3, b3) = cache
+    
+    dZ3 = A3 - Y
+    dW3 = 1./m * np.dot(dZ3, A2.T)
+    db3 = 1./m * np.sum(dZ3, axis=1, keepdims = True)
+    dA2 = np.dot(W3.T, dZ3)
+
+    dA2 = np.multiply(dA2,D2)        # Step 1: Apply mask D2 to shut down the same neurons as during the forward propagation
+    dA2 = np.divide(dA2,keep_prob)   # Step 2: Scale the value of neurons that haven't been shut down
+
+    dZ2 = np.multiply(dA2, np.int64(A2 > 0))
+    dW2 = 1./m * np.dot(dZ2, A1.T)
+    db2 = 1./m * np.sum(dZ2, axis=1, keepdims = True)
+    
+    dA1 = np.dot(W2.T, dZ2)
+
+    dA1 = np.multiply(dA1,D1)         # Step 1: Apply mask D1 to shut down the same neurons as during the forward propagation
+    dA1 = np.divide(dA1,keep_prob)    # Step 2: Scale the value of neurons that haven't been shut down
+
+    dZ1 = np.multiply(dA1, np.int64(A1 > 0))
+    dW1 = 1./m * np.dot(dZ1, X.T)
+    db1 = 1./m * np.sum(dZ1, axis=1, keepdims = True)
+    
+    gradients = {"dZ3": dZ3, "dW3": dW3, "db3": db3,"dA2": dA2,
+                 "dZ2": dZ2, "dW2": dW2, "db2": db2, "dA1": dA1, 
+                 "dZ1": dZ1, "dW1": dW1, "db1": db1}
+    
+    return gradients
+   
+# ==================================================================    
+
+
 def L_layer_model(X, Y, layers_dims, learning_rate = 0.0075, num_iterations = 3000, print_cost=False):#lr was 0.009
     """
     Implements a L-layer neural network: [LINEAR->RELU]*(L-1)->LINEAR->SIGMOID.
