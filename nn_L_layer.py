d6994b1e07074699b9a71818d936b9e32d152360
Adam Jonze <ajze@users.noreply.github.com>
2018-01-31 12:16:44 +0000

    Update nn_L_layer.py

--- nn_L_layer.py
+++ nn_L_layer.py
@@ -395,6 +395,65 @@ def backward_propagation_with_dropout(X, Y, cache, keep_prob):
    
 # ==================================================================    
 
+# ==================================================================
+## Need to implement the vec->dict, dict->vec, grad->vec funcs  
+
+def gradient_check_n(parameters, gradients, X, Y, epsilon = 1e-7):
+    """
+    Checks if backward_propagation_n computes correctly the gradient of the cost output by forward_propagation_n
+    
+    Arguments:
+    parameters -- python dictionary containing your parameters "W1", "b1", "W2", "b2", "W3", "b3":
+    grad -- output of backward_propagation_n, contains gradients of the cost with respect to the parameters. 
+    x -- input datapoint, of shape (input size, 1)
+    y -- true "label"
+    epsilon -- tiny shift to the input to compute approximated gradient with formula(1)
+    
+    Returns:
+    difference -- difference (2) between the approximated gradient and the backward propagation gradient
+    """
+    
+    # Set-up variables
+    parameters_values, _ = dictionary_to_vector(parameters)
+    grad = gradients_to_vector(gradients)
+    num_parameters = parameters_values.shape[0]
+    J_plus = np.zeros((num_parameters, 1))
+    J_minus = np.zeros((num_parameters, 1))
+    gradapprox = np.zeros((num_parameters, 1))
+    
+    # Compute gradapprox
+    for i in range(num_parameters):
+        
+        # Compute J_plus[i]. Inputs: "parameters_values, epsilon". Output = "J_plus[i]".
+        # "_" is used because the function you have to outputs two parameters but we only care about the first one
+        thetaplus = np.copy(parameters_values)                                      
+        thetaplus[i][0] = thetaplus[i][0] + epsilon                                
+        J_plus[i], _ = forward_propagation_n(X, Y, vector_to_dictionary(thetaplus))  
+        
+        # Compute J_minus[i]. Inputs: "parameters_values, epsilon". Output = "J_minus[i]".
+        thetaminus = np.copy(parameters_values)                                      
+        thetaminus[i][0] = thetaminus[i][0] - epsilon                                       
+        J_minus[i], _ = forward_propagation_n(X, Y, vector_to_dictionary(thetaminus)) 
+        
+        # Compute gradapprox[i]
+        gradapprox[i] = (J_plus[i] - J_minus[i]) / (2 * epsilon)
+    
+    # Compare gradapprox to backward propagation gradients by computing difference.
+    numerator = np.linalg.norm(grad - gradapprox)                                           
+    denominator = (np.linalg.norm(grad)) + (np.linalg.norm(gradapprox))                                         
+    difference = numerator/denominator  # Step 3'
+
+    if difference > 2e-7:
+        print ("\033[93m" + "There is a mistake in the backward propagation! difference = " + str(difference) + "\033[0m")
+    else:
+        print ("\033[92m" + "Your backward propagation works perfectly fine! difference = " + str(difference) + "\033[0m")
+    
+    return difference
+
+# ==================================================================  
+
+# ==================================================================  
+## Model
 
 def L_layer_model(X, Y, layers_dims, learning_rate = 0.0075, num_iterations = 3000, print_cost=False):#lr was 0.009
     """
